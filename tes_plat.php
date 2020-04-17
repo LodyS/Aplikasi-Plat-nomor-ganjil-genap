15d6ac4f895118f7be8935ce55f93ad8520ceffc
LodyS <39235236+LodyS@users.noreply.github.com>
2020-04-17 10:31:06 +0700

    Create tes_plat.php

---
+++ tes_plat.php
@@ -0,0 +1,4 @@
+<h1>Aplikasi plat nomor ganjil-genap</h1>
+<form action="plat.php" method="post">
+Plat nomor kendaraan Anda : <input type="text" name="plat_nomor" required><br/>
+<input type="submit" value="Cek">
